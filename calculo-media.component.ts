5733af7cfeeeeccaff2a8cc026d28cc39565bcd8
Rodrigo Lopes <142754613+RodrigoLopes11@users.noreply.github.com>
2023-11-14 21:35:20 -0300

    Update calculo-media.component.ts

--- calculo-media.component.ts
+++ calculo-media.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit} from '@angular/core';
   styleUrls: ['./calculo-media.component.css']
 })
 
-
+ngOnInit(){
+}
 export class CalculoMediaComponent implements OnInit {
   ac1: number|any;
   ac2: number|any;
